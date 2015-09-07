0dda47faae98a4107e831e096c878694580737d2
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2015-09-07 19:04:26 +0200

    s/yum/dnf/

--- fish/config.fish
+++ fish/config.fish
@@ -8,10 +8,12 @@ for item in $HOME/.local/bin node_modules/.bin
     end
 end
 
+
+
 set -x WORKON_HOME $HOME/.local/venvs
 set -x PKG_CONFIG_PATH $HOME/.local/lib/pkgconfig
 
-set SUDO_ALIASES btrfs pm-suspend yum
+set SUDO_ALIASES btrfs pm-suspend dnf
 
 for com in $SUDO_ALIASES
     alias $com="sudo $com"
